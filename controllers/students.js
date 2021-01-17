ebe14853bb8f29257be3417c0cd706035aea8c97
SariMelina <mmkings_beba@hotmail.com>
2021-01-17 17:41:49 -0600

    Making a validate for 3 objects

--- controllers/students.js
+++ controllers/students.js
@@ -10,9 +10,14 @@ function postStudent (body){
 function validate(body){
     let ok = false
     if(body && typeof body === 'object' && Object.keys(body).length > 0){
-        if(body.hasOwnProperty('name') && body.name !== '' && body.length > 3){
-            ok = true
+        if(body.hasOwnProperty('name') && body.name !== '' && body.name.length > 3){
+            if(body.hasOwnProperty('phone') && body.phone !== '' && body.phone.length === 10){
+                if(body.hasOwnProperty('gender') && body.gender !== '' && body.gender.length === 1 && (body.gender === 'F' || body.gender === 'M')){
+                    ok = true
+                }
+            }
         }
+
     }
     return ok
 }
